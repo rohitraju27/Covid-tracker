afd32e2df6d28bee8d39a6372ef6449c94d7a351
unknown <rohu270700@gmail.com>
2020-08-10 12:46:10 +0530

    Updates

---
+++ precache-manifest.5cfdd82b42224bf7cd9af1619b166ce4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85525a20aaecbc6c513bf989b4e775ab",
+    "url": "/Covid-tracker/index.html"
+  },
+  {
+    "revision": "fa27b1971b22684b75b0",
+    "url": "/Covid-tracker/static/css/main.201e66b0.chunk.css"
+  },
+  {
+    "revision": "f045e11e516c218944ed",
+    "url": "/Covid-tracker/static/js/2.11fca403.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/Covid-tracker/static/js/2.11fca403.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa27b1971b22684b75b0",
+    "url": "/Covid-tracker/static/js/main.89ed4a6e.chunk.js"
+  },
+  {
+    "revision": "5c57e46c63087f7431c7",
+    "url": "/Covid-tracker/static/js/runtime-main.ee52fc7c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid-tracker/static/media/image.d7265326.png"
+  }
+]);
